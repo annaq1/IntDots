1d6d1c9b864ce8864b2310b9e7a9c8e57d81feeb
annaq1 <annaq1@uci.edu>
2018-02-28 19:02:46 -0800

    initial commit

---
+++ Assets/Scripts/ClickOnDots.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOnDots : MonoBehaviour {
+
+
+    //Destroy GameObject if you click the mouse on it
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("COLLISION");
+        Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private float playerSpeed = 5; //Give player some speed so its not slow af
+
+    void MovePlayer()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("UP");
+            transform.Translate(0, playerSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("DOWN");
+            transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("LEFT");
+            transform.Translate(-playerSpeed * Time.deltaTime, 0 ,0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("RIGHT");
+            transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MovePlayer();
+		
+	}
+}
